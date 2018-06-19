14ddae86d3dc17971ba33d1de58498c5fa251593
Jarda Snajdr <jsnajdr@gmail.com>
2018-06-19 11:33:34 +0200

    Comment out Gutenberg API deprecation calls: they access wp.blocks global

--- vendor/gutenberg/editor/deprecated.js
+++ vendor/gutenberg/editor/deprecated.js
@@ -6,7 +6,7 @@ import { forEach } from 'lodash';
 /**
  * WordPress dependencies
  */
-import { Component } from '@wordpress/element';
+import React, { Component } from 'react';
 import deprecated from '@wordpress/deprecated';
 import { withSelect } from '@wordpress/data';
 
@@ -67,49 +67,49 @@ const functionsToDeprecate = {
 	editorMediaUpload,
 };
 
-forEach( componentsToDepreacate, ( WrappedComponent, key ) => {
-	wp.blocks[ key ] = class extends Component {
-		constructor() {
-			super( ...arguments );
-
-			deprecated( 'wp.blocks.' + key, {
-				version: '3.1',
-				alternative: 'wp.editor.' + key,
-				plugin: 'Gutenberg',
-			} );
-		}
-
-		render() {
-			return <WrappedComponent { ...this.props } />;
-		}
-	};
-} );
-
-forEach( functionsToDeprecate, ( wrappedFunction, key ) => {
-	wp.blocks[ key ] = ( ...args ) => {
-		deprecated( 'wp.blocks.' + key, {
-			version: '3.1',
-			alternative: 'wp.editor.' + key,
-			plugin: 'Gutenberg',
-		} );
-		return wrappedFunction( ...args );
-	};
-} );
-
-wp.blocks.withEditorSettings = ( mapSettingsToProps ) => ( WrappedComponent ) => {
-	const applyWithSelect = withSelect( ( select, ownProps ) => {
-		const settings = select( 'core/editor' ).getEditorSettings();
-		if ( ! mapSettingsToProps ) {
-			return { settings };
-		}
-		return mapSettingsToProps( settings, ownProps );
-	} );
-
-	deprecated( 'wp.blocks.withEditorSettings', {
-		version: '3.1',
-		alternative: 'the data module to access the editor settings `wp.data.select( "core/editor" ).getEditorSettings()`',
-		plugin: 'Gutenberg',
-	} );
-
-	return applyWithSelect( WrappedComponent );
-};
+// forEach( componentsToDepreacate, ( WrappedComponent, key ) => {
+// 	wp.blocks[ key ] = class extends Component {
+// 		constructor() {
+// 			super( ...arguments );
+// 
+// 			deprecated( 'wp.blocks.' + key, {
+// 				version: '3.1',
+// 				alternative: 'wp.editor.' + key,
+// 				plugin: 'Gutenberg',
+// 			} );
+// 		}
+// 
+// 		render() {
+// 			return <WrappedComponent { ...this.props } />;
+// 		}
+// 	};
+// } );
+// 
+// forEach( functionsToDeprecate, ( wrappedFunction, key ) => {
+// 	wp.blocks[ key ] = ( ...args ) => {
+// 		deprecated( 'wp.blocks.' + key, {
+// 			version: '3.1',
+// 			alternative: 'wp.editor.' + key,
+// 			plugin: 'Gutenberg',
+// 		} );
+// 		return wrappedFunction( ...args );
+// 	};
+// } );
+// 
+// wp.blocks.withEditorSettings = ( mapSettingsToProps ) => ( WrappedComponent ) => {
+// 	const applyWithSelect = withSelect( ( select, ownProps ) => {
+// 		const settings = select( 'core/editor' ).getEditorSettings();
+// 		if ( ! mapSettingsToProps ) {
+// 			return { settings };
+// 		}
+// 		return mapSettingsToProps( settings, ownProps );
+// 	} );
+// 
+// 	deprecated( 'wp.blocks.withEditorSettings', {
+// 		version: '3.1',
+// 		alternative: 'the data module to access the editor settings `wp.data.select( "core/editor" ).getEditorSettings()`',
+// 		plugin: 'Gutenberg',
+// 	} );
+// 
+// 	return applyWithSelect( WrappedComponent );
+// };
